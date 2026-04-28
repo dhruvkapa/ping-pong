e46e3347bd669a9e4f77fa46c4b83b2ddb09e391
Dhruv Kapadia <dhruvkapadia01@hotmail.com>
2026-04-27 22:27:22 -0400

    Added paddles and ping pong ball

--- pong.js
+++ pong.js
@@ -1,1 +1,31 @@
- 
+var canvas = document.querySelector('canvas');
+const ctx = canvas.getContext("2d");
+
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Draw the ball
+ctx.beginPath();
+ctx.arc(window.innerWidth/2, window.innerHeight/2, 10, 0, 2 * Math.PI);
+
+ctx.fillStyle = "white";
+ctx.fill();
+
+ctx.stroke();
+
+//Leftside Paddle (Human Player)
+ctx.beginPath();
+ctx.rect(0, window.innerHeight/2, 10, 50);
+ctx.fillStyle = "white";
+ctx.fill();
+ctx.stroke();
+
+
+
+//Rightside Paddle (Computer Player)
+ctx.beginPath();
+ctx.rect(canvas.width-25, window.innerHeight/2, 10, 50);
+ctx.fillStyle = "white";
+ctx.fill();
+ctx.stroke();
